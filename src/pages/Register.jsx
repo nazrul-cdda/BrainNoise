2e898245a3c72627a28b26c3f902cbf22b8dc007
nazrul-cdda <nazrul.cdda@gmail.com>
2026-04-20 18:32:36 +0600

    added title

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -38,20 +38,69 @@ export const Register = () => {
                 value = {Name}
                 placeholder="Full Name"
                 onChange={(e) => setName(e.target.value)}
+                style={{
+                    marginLeft: "20px",
+                    resize: "vertical",
+                    padding: "12px",
+                    borderRadius: "14px",
+                    border: "2px solid #e8c6a6", 
+                    fontSize: "14px",
+                    outline: "none",
+                    backgroundColor: "#fff3e6",
+                    color: "#5a4633",
+                    boxShadow: "0 4px 12px rgba(232, 198, 166, 0.35)"
+                }}
             />
             <input
                 type = "username"
                 value = {username}
                 placeholder="username"
                 onChange={(e) => setUsername(e.target.value)}
+                style={{
+                    marginLeft: "20px",
+                    resize: "vertical",
+                    padding: "12px",
+                    borderRadius: "14px",
+                    border: "2px solid #e8c6a6", 
+                    fontSize: "14px",
+                    outline: "none",
+                    backgroundColor: "#fff3e6",
+                    color: "#5a4633",
+                    boxShadow: "0 4px 12px rgba(232, 198, 166, 0.35)"
+                }}
             />
             <input
                 type = "password"
                 value = {password}
                 placeholder="password"
                 onChange={(e) => setPassword(e.target.value)}
+                style={{
+                    marginLeft: "20px",
+                    resize: "vertical",
+                    padding: "12px",
+                    borderRadius: "14px",
+                    border: "2px solid #e8c6a6", 
+                    fontSize: "14px",
+                    outline: "none",
+                    backgroundColor: "#fff3e6",
+                    color: "#5a4633",
+                    boxShadow: "0 4px 12px rgba(232, 198, 166, 0.35)"
+                }}
             />
-            <button onClick={addToUser}>Registration</button>
+            <button 
+            style={{
+                    marginLeft: "20px",
+                    resize: "vertical",
+                    padding: "12px",
+                    borderRadius: "14px",
+                    border: "2px solid #e8c6a6", 
+                    fontSize: "14px",
+                    outline: "none",
+                    backgroundColor: "#fff3e6",
+                    color: "#5a4633",
+                    boxShadow: "0 4px 12px rgba(232, 198, 166, 0.35)"
+                }}
+            onClick={addToUser}>Registration</button>
         </div>
     )
 }
